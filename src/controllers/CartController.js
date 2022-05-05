d8f5d5741e0796a2ac534cb196d5271efb7dafd2
Aakash Yadav <82597490+aakash-0@users.noreply.github.com>
2022-05-05 11:29:24 +0530

    added cart controllers

--- src/controllers/CartController.js
+++ src/controllers/CartController.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const User = require("../models/UserModel");
-const Product = require("../models/ProductsModel");
 const authenticate = require("../middlewares/authenticate");
-const testingUser = require("../middlewares/testingUser");
+
 const router = express.Router();
 
-router.post("/:id",authenticate,testingUser,async(req,res)=>{
+router.post("/:id",authenticate,async(req,res)=>{
     try {
         await req.user.addToCart(req.params.id)
         res.redirect("/")
@@ -15,7 +13,7 @@ router.post("/:id",authenticate,testingUser,async(req,res)=>{
     }
     
 })
-router.post("/remove/:id",authenticate,testingUser,async (req,res)=>{
+router.post("/remove/:id",authenticate,async (req,res)=>{
     try {
         await req.user.removeCartItem(req.params.id)
         res.redirect("/");
@@ -23,7 +21,7 @@ router.post("/remove/:id",authenticate,testingUser,async (req,res)=>{
         console.log(error);
     }
 })
-router.post("/delete/:id",authenticate,testingUser,async (req,res)=>{
+router.post("/delete/:id",authenticate,async (req,res)=>{
     try {
         await req.user.deleteCartItem(req.params.id)
         res.redirect("/");
