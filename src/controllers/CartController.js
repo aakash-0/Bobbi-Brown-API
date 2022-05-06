11a9ea295d6e3f5b29cea6f0a89195f764b1260d
Aakash Yadav <82597490+aakash-0@users.noreply.github.com>
2022-05-06 09:25:36 +0530

    fixed and updated controller

--- src/controllers/CartController.js
+++ src/controllers/CartController.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const User = require("../models/UserModel");
 const authenticate = require("../middlewares/authenticate");
 
 const router = express.Router();
 
 router.post("/:id",authenticate,async(req,res)=>{
     try {
-        await req.user.addToCart(req.params.id)
+        
+        let user =await User.findById(req.user._id);
+        await user.addToCart(req.params.id)
         res.redirect("/")
         
     } catch (error) {
@@ -15,7 +18,8 @@ router.post("/:id",authenticate,async(req,res)=>{
 })
 router.post("/remove/:id",authenticate,async (req,res)=>{
     try {
-        await req.user.removeCartItem(req.params.id)
+        let user =await User.findById(req.user._id);
+        await user.removeCartItem(req.params.id)
         res.redirect("/");
     } catch (error) {
         console.log(error);
@@ -23,7 +27,8 @@ router.post("/remove/:id",authenticate,async (req,res)=>{
 })
 router.post("/delete/:id",authenticate,async (req,res)=>{
     try {
-        await req.user.deleteCartItem(req.params.id)
+        let user =await User.findById(req.user._id);
+        await user.deleteCartItem(req.params.id)
         res.redirect("/");
     } catch (error) {
         console.log(error);
